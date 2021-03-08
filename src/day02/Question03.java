cd88f0d8c731edbb72a43ba75bb022c0397048e4
Ebubekir <esahin.techproed@gmail.com>
2021-03-08 22:27:11 +0300

    Second Week

---
+++ src/day02/Question03.java
@@ -0,0 +1,10 @@
+package day02;
+/* Write a return method that accepts an integer as input
+ * and calculates factorial and prints like output
+ *
+ * Eg : Input : 6
+ *      Output: 6!=6*5*4*3*2*1=720
+ */
+public class Question03 {
+
+}
---
+++ src/day02/Question04.java
@@ -0,0 +1,8 @@
+package day02;
+//ask user to enter an e-mail address
+// then if it contains "hotmail" replace it with "gmail"
+// e.g: johnbrown@hotmail.com ==> johnbrown@gmail.com
+
+public class Question04 {
+
+}
